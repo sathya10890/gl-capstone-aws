97f7e55e16b409880c81214fbcfd803d700634e0
Sathya Natarajan <honey.sathya2@gmail.com>
2021-10-10 23:35:45 +0530

    update app.py with more logs and fix to pass the test case

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "".join((random_string))
+    LOGGER.info('Received a message: %s', random_string)
+    return "".join(reversed(random_string))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
